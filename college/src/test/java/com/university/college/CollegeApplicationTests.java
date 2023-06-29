779204f3eb41d0c7aa62413632381296da9b7b5b
Sairam Chekuri <sairamc3@gmail.com>
2023-06-29 22:46:38 +0530

    Adding the college for oAuth Work

---
+++ college/src/test/java/com/university/college/CollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.college;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
